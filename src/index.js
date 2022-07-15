9bf8074643e82d9ae9cbc969ec751b97acaebc32
Maksym32 <bednogo1@gmail.com>
2022-07-15 10:15:10 +0300

    hw11

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const btn = document.querySelector('.load-more-btn')
 
 form.addEventListener('submit', onFormSubmit);
 btn.addEventListener('click', onLoadMoreBtnClick);
-btn.classList.add('is-hidden')
+btn.classList.add('is-hidden');
+searchText = '';
 
 async function onFormSubmit(e) {
   e.preventDefault();
